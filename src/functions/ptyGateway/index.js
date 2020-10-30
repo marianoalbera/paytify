c8cd786de2f0e670e8ad2c7f16197d33567c238c
Mariano Albera <malbera@gmail.com>
2020-10-30 10:34:38 +0000

    Mocking the cards tokenize endpoint now

--- src/functions/ptyGateway/index.js
+++ src/functions/ptyGateway/index.js
@@ -12,17 +12,24 @@
 let express = require('express');
 let app = express();
 
+var dateFormat = require('dateformat');
+var now = new Date();
+var exp = new Date();
+exp.setHours(exp.getHours()+1);
+
+
+//I need to validate token in header
+
+//and I route through the different functions
+app.post('/cards/tokenize', (req,res) => {
+  res.setHeader('Content-Type', 'application/json');
+  res.status(200).send('{id:"zDzgTJThFCaUojhRvFn9MGF22DOdeSwP",created:"'
+    +dateFormat(now, "yyyymmdd hh:MM:ss")+'", expiration:"'
+    +dateFormat(exp, "yyyymmdd hh:MM:ss")+'"}');
+});
+
 app.get('/', (req,res) => {
-  res.status(200).send('Hello GET Yes and again!');
-});
-app.post('/', (req,res) => {
-  res.status(200).send('Hello POST!');
-});
-app.put('/', (req,res) => {
-  res.status(200).send('Hello PUT!');
-});
-app.delete('/', (req,res) => {
-  res.status(200).send('Hello DELETE!');
+  res.status(200).send('All your base are belong to us');
 });
 
 exports.paytify = app;
